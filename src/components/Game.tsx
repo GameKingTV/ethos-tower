3ea930936e6b8f64518fd1e9f93afec7dea47187
Sametx <samet-x_@hotmail.com>
2025-08-01 02:35:14 +0300

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -49,7 +49,7 @@ if (topPlatformY > 0) {
           player.x + player.width > plat.x &&
           player.x < plat.x + plat.width
         ) {
-          player.vy = -3;
+          player.vy = -8;
         }
       }
 
