dae70681fae6aabadd6c01389b0180ffa322e768
Sametx <samet-x_@hotmail.com>
2025-08-01 02:05:56 +0300

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Game from '@/components/Game';
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center text-3xl font-bold mt-6">Ethos tower</h1>
+      <Game />
+    </main>
+  );
+}
