924933d050794347ec9e2b7164e5d8c8491a8588
Sametx <samet-x_@hotmail.com>
2025-08-01 04:23:21 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,46 @@ import Game from '@/components/Game';
 
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', fontFamily: "'Comic Neue', cursive" }}>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }}>
+    <div
+      style={{
+        backgroundColor: '#d0eaff',
+        minHeight: '100vh',
+        fontFamily: "'Comic Neue', cursive",
+        position: 'relative',
+      }}
+    >
+      {/* Üst Başlık */}
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingTop: '20px',
+        }}
+      >
         <h1 style={{ marginRight: '10px', fontSize: '36px' }}>Ethos Tower</h1>
         <img src="/ethos-logo.png" alt="Logo" style={{ height: '75px' }} />
       </div>
+
+      {/* Sağ üst köşe Twitter linki */}
+      <a
+        href="https://x.com/sametx123" // ← kendi Twitter linkinle değiştir
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          position: 'absolute',
+          top: '20px',
+          right: '20px',
+        }}
+      >
+        <img
+          src="/twitter.png" // ← twitter logonu public klasörüne eklediğini varsayıyorum
+          alt="Twitter"
+          style={{ width: '36px', height: '36px' }}
+        />
+      </a>
+
+      {/* Oyun Alanı */}
       <Game />
     </div>
   );
