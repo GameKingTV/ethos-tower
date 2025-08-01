d0bb2db393d4710b2b771a93393e492555982e89
Sametx <samet-x_@hotmail.com>
2025-08-01 04:10:03 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+'use client';
 import Game from '@/components/Game';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Ethos Tower</h1>
+    <div style={{ textAlign: 'center', fontFamily: "'Comic Neue', cursive" }}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }}>
+        <h1 style={{ marginRight: '10px', fontSize: '36px' }}>Ethos Tower</h1>
+        <img src="/ethos-logo.png" alt="Logo" style={{ height: '40px' }} />
+      </div>
       <Game />
     </div>
   );
