4ce96afab497855cc1a05e227c3c2cefe0b48dce
Sametx <samet-x_@hotmail.com>
2025-08-01 04:11:41 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div style={{ textAlign: 'center', fontFamily: "'Comic Neue', cursive" }}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }}>
         <h1 style={{ marginRight: '10px', fontSize: '36px' }}>Ethos Tower</h1>
-        <img src="/ethos-logo.png" alt="Logo" style={{ height: '40px' }} />
+        <img src="/ethos-logo.png" alt="Logo" style={{ height: '75px' }} />
       </div>
       <Game />
     </div>
