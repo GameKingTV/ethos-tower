544e1f48f62aebc4a4f057fc0c534bf5d86f1e45
Sametx <samet-x_@hotmail.com>
2025-08-16 22:27:01 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,8 @@
 'use client';
-import Game from '@/components/Game';
+import dynamic from 'next/dynamic';
+
+const Menu = dynamic(() => import('@/components/Menu'), { ssr: false });
 
 export default function Home() {
-  return (
-    <div
-      style={{
-        backgroundColor: '#9de471',
-        minHeight: '100vh',
-        fontFamily: "'Comic Neue', cursive",
-        position: 'relative',
-      }}
-    >
-      {/* Üst Başlık */}
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          paddingTop: '20px',
-        }}
-      >
-        <h1 style={{ marginRight: '10px', fontSize: '36px' }}>Eth OS Tower</h1>
-        <img src="/ethos-logo.png" alt="Logo" style={{ height: '75px' }} />
-      </div>
-
-      {/* Sağ üst köşe Twitter linki */}
-      <a
-        href="https://x.com/sametx123" // ← kendi Twitter linkinle değiştir
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          position: 'absolute',
-          top: '20px',
-          right: '20px',
-        }}
-      >
-        <img
-          src="/twitter.png" // ← twitter logonu public klasörüne eklediğini varsayıyorum
-          alt="Twitter"
-          style={{ width: '36px', height: '36px' }}
-        />
-      </a>
-
-      {/* Oyun Alanı */}
-      <Game />
-    </div>
-  );
+  return <Menu />;
 }
